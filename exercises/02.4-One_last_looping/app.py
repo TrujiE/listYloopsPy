c5e282ea295fdb14b657994d11c5238167a08a1c
TrujiE <atrujilloembry@gmail.com>
2021-04-12 01:16:06 +0000

    listloops

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,4 +1,10 @@
 my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 #Your code here:
-
+largo=len(my_sample_list)
+#print(largo)
+my_sample_list[1]="Steven"
+my_sample_list[10]="Pepe"
+my_sample_list[0]=my_sample_list[2]+my_sample_list[4]
+for largo in range(largo-1,-1,-1):
+    print(my_sample_list[largo])
