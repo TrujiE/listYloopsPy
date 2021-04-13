1787aa2f6dbd3dcd6a3a346064f7cf25135a164f
TrujiE <atrujilloembry@gmail.com>
2021-04-13 01:44:24 +0000

    listloop2

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -1,9 +1,17 @@
 people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 
 #Your code go here:
+people_temp=[]
+people_temp=people
 def deletePerson(person_name):
     #Your code go here:
-    
+    for x,y in enumerate(people_temp):
+        if y == person_name:
+            people_temp.pop(x)
+ 
+    return people_temp
+   
+
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
 print(deletePerson("emilio"))
