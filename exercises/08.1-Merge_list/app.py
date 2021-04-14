7b8e3d30b6441f20beb5cd5699031d380ee4daec
TrujiE <atrujilloembry@gmail.com>
2021-04-14 00:37:54 +0000

    listloop3

--- exercises/08.1-Merge_list/app.py
+++ exercises/08.1-Merge_list/app.py
@@ -4,5 +4,9 @@ chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas']
 
 def merge_list(list1, list2):
     #Your code go here:
+    union = []
+    union=list1 + list2
+    
+    return union
     
 print(merge_list(chunk_one, chunk_two))
