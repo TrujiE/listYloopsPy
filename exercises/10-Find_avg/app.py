19475631c1e00d5121ae6d5e7dccc0d7249b4e1a
TrujiE <atrujilloembry@gmail.com>
2021-04-14 02:05:44 +0000

    listloop4

--- exercises/10-Find_avg/app.py
+++ exercises/10-Find_avg/app.py
@@ -1,4 +1,14 @@
 my_list = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423]
 
 #Your code here:
+cero=0
+largo=len(my_list)
+#print(largo)
+for x in my_list:
+    cero= cero + x
 
+#print(cero)
+prom = cero / largo
+
+print(prom)
+    
